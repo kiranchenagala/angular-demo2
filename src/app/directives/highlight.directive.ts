518f99e10d60cf298d3c76990350f9deb6950b57
unknown <kiranchenagala@gmail.com>
2018-07-10 11:38:48 +0530

    commit

--- src/app/directives/highlight.directive.ts
+++ src/app/directives/highlight.directive.ts
@@ -1,12 +1,29 @@
-import { Directive, ElementRef } from '@angular/core'
+import { Directive, ElementRef, HostListener, HostBinding, style, Input } from '@angular/core'
 
 @Directive({
     selector : '[apphighlight]'
 })
 
 export class HighlightDirective{
+
+    @Input('myColor') myColor : string;
+
+    @HostBinding('style.backgroundColor') bgColor : string;
+
+    @HostListener('mouseenter') mouseenter(){
+        // alert('Mouse entered');
+        // this.elementRef.nativeElement.style.backgroundColor = "aqua";
+        this.bgColor = this.myColor;
+    }
+
+    @HostListener('mouseleave') mouseleave(){
+        // alert('Mouse leaving');
+        // this.elementRef.nativeElement.style.backgroundColor = "transparent";
+        this.bgColor = "transparent";
+    }
+
    constructor(private elementRef : ElementRef){
-       this.elementRef.nativeElement.style.backgroundColor = "aqua";
+    //    this.elementRef.nativeElement.style.backgroundColor = "aqua";
     //    console.log(this.elementRef.nativeElement);
    }
 }
